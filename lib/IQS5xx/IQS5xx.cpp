f5285d1a03230b303f77a51352647504b85cab5c
WolfessNaomi <61919699+wolfessnaomitest@users.noreply.github.com>
2026-06-26 22:49:47 -0500

    Update IQS5xx.cpp

--- lib/IQS5xx/IQS5xx.cpp
+++ lib/IQS5xx/IQS5xx.cpp
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2025 Mariano Uvalle
+ * SPDX-License-Identifier: MIT
+ */
+
+//MIT License
+
+//Copyright (c) 2025 Mariano Uvalle
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
 #include "IQS5xx.h"
 
 IQS5xx::IQS5xx() : _wire(nullptr), _addr(0x74), _rdy_pin(-1), _rst_pin(-1), _rdy_active_high(false) {}
